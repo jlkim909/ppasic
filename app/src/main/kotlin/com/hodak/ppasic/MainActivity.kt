b4d978e214a9d1ba8825f6e75f76ae50eed03cf2
hodak <jlkim909@gmail.com>
2024-06-12 14:04:41 +0900

    feat : initial HoApp (#3)

--- app/src/main/kotlin/com/hodak/ppasic/MainActivity.kt
+++ app/src/main/kotlin/com/hodak/ppasic/MainActivity.kt
@@ -20,44 +20,28 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.hodak.ppasic.ui.theme.PpasicTheme
+import androidx.compose.runtime.CompositionLocalProvider
+import com.hodak.ppasic.core.data.util.NetworkMonitor
+import com.hodak.ppasic.core.designsystem.theme.HoTheme
+import com.hodak.ppasic.ui.HoApp
+import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+
+    @Inject
+    lateinit var networkMonitor: NetworkMonitor
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            PpasicTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding),
-                    )
+            CompositionLocalProvider() {
+                HoTheme {
+                    HoApp()
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier,
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    PpasicTheme {
-        Greeting("Android")
     }
 }
