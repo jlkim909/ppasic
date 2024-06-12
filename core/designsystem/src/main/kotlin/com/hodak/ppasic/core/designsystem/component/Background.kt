0017cb0665bfe9321f38d8e96bd47578b6c32e19
hodak <jlkim909@gmail.com>
2024-06-12 14:27:20 +0900

    feat : HoBackground 구현 (#3)

---
+++ core/designsystem/src/main/kotlin/com/hodak/ppasic/core/designsystem/component/Background.kt
@@ -0,0 +1,71 @@
+/*
+ * Copyright 2024 hodak
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.hodak.ppasic.core.designsystem.component
+
+import android.content.res.Configuration
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.LocalAbsoluteTonalElevation
+import androidx.compose.material3.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+import com.hodak.ppasic.core.designsystem.theme.HoTheme
+import com.hodak.ppasic.core.designsystem.theme.LocalBackgroundTheme
+
+@Composable
+fun HoBackground(
+    modifier: Modifier = Modifier,
+    content: @Composable () -> Unit,
+) {
+    val color = LocalBackgroundTheme.current.color
+    val tonalElevation = LocalBackgroundTheme.current.tonalElevation
+
+    Surface(
+        color = if (color == Color.Unspecified) Color.Transparent else color,
+        tonalElevation = if (tonalElevation == Dp.Unspecified) 0.dp else tonalElevation,
+        modifier = modifier.fillMaxSize(),
+    ) {
+        CompositionLocalProvider(value = LocalAbsoluteTonalElevation provides 0.dp) {
+            content()
+        }
+    }
+}
+
+@Preview(uiMode = Configuration.UI_MODE_NIGHT_NO, name = "Light theme")
+@Preview(uiMode = Configuration.UI_MODE_NIGHT_YES, name = "Dark theme")
+annotation class ThemePreviews
+
+@ThemePreviews
+@Composable
+fun BackgroundDefault() {
+    HoTheme(disableDynamicTheming = true) {
+        HoBackground(Modifier.size(100.dp), content = {})
+    }
+}
+
+@ThemePreviews
+@Composable
+fun BackgroundDynamic() {
+    HoTheme(disableDynamicTheming = false) {
+        HoBackground(Modifier.size(100.dp), content = {})
+    }
+}
