b179c35e2de481297a28bdbc5b2ecf475f02d0d0
hodak <jlkim909@gmail.com>
2024-06-12 19:46:16 +0900

    feat : HoIconToggleButton 구현 (#3)

---
+++ core/designsystem/src/main/kotlin/com/hodak/ppasic/core/designsystem/component/IconButton.kt
@@ -0,0 +1,110 @@
+/*
+ * Copyright 2024 hodak
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.hodak.ppasic.core.designsystem.component
+
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.icons.filled.FavoriteBorder
+import androidx.compose.material3.FilledIconToggleButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButtonDefaults
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import com.hodak.ppasic.core.designsystem.theme.HoTheme
+
+@Composable
+fun HoIconToggleButton(
+    checked: Boolean,
+    onCheckedChange: (Boolean) -> Unit,
+    modifier: Modifier = Modifier,
+    enabled: Boolean = true,
+    icon: @Composable () -> Unit,
+    checkedIcon: @Composable () -> Unit = icon,
+) {
+    FilledIconToggleButton(
+        checked = checked,
+        onCheckedChange = onCheckedChange,
+        modifier = modifier,
+        enabled = enabled,
+        colors = IconButtonDefaults.iconToggleButtonColors(
+            checkedContainerColor = MaterialTheme.colorScheme.primaryContainer,
+            checkedContentColor = MaterialTheme.colorScheme.onPrimaryContainer,
+            disabledContainerColor = if (checked) {
+                MaterialTheme.colorScheme.onBackground.copy(
+                    alpha = HoIconButtonDefaults.DISABLED_ICON_BUTTON_CONTAINER_ALPHA,
+                )
+            } else {
+                Color.Transparent
+            },
+        ),
+    ) {
+        if (checked) checkedIcon() else icon()
+    }
+}
+
+@ThemePreviews
+@Composable
+fun IconButtonPreview() {
+    HoTheme {
+        HoIconToggleButton(
+            checked = true,
+            onCheckedChange = {},
+            icon = {
+                Icon(
+                    imageVector = Icons.Default.FavoriteBorder,
+                    contentDescription = null,
+                )
+            },
+            checkedIcon = {
+                Icon(
+                    imageVector = Icons.Default.Favorite,
+                    contentDescription = null,
+                )
+            },
+        )
+    }
+}
+
+@ThemePreviews
+@Composable
+fun IconButtonPreviewUnchecked() {
+    HoTheme {
+        HoIconToggleButton(
+            checked = false,
+            onCheckedChange = {},
+            icon = {
+                Icon(
+                    imageVector = Icons.Default.FavoriteBorder,
+                    contentDescription = null,
+                )
+            },
+            checkedIcon = {
+                Icon(
+                    imageVector = Icons.Default.Favorite,
+                    contentDescription = null,
+                )
+            },
+        )
+    }
+}
+
+
+object HoIconButtonDefaults {
+    const val DISABLED_ICON_BUTTON_CONTAINER_ALPHA = 0.12f
+}
